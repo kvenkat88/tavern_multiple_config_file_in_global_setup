629e04bc31e235bb75b0a78f40f54008bcafb2e7
kvenkat88 <kvenkatkrisit@gmail.com>
2018-12-21 18:03:57 +0530

    added additional tavern feature tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,8 @@ import pytest
 #Refer the below mentioned link for goog docstring implementation
 # https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 
+import pytest
+
+@pytest.mark.welcome
+def test_send_welcome():
+    return "Welcome"
