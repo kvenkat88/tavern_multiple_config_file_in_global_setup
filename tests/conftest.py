9b0ee4f90d9c994c0506f8305d297ee5d82cd6e0
kvenkat88 <kvenkatkrisit@gmail.com>
2018-12-24 12:23:18 +0530

    additional tests added for learning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,17 @@
-import pytest
-
 #Refer the below mentioned link for goog docstring implementation
 # https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 
 import pytest
+import logging
+import time
 
 @pytest.mark.welcome
 def test_send_welcome():
-    return "Welcome"
+    yield "Welcome"
+
+@pytest.fixture(name="time_request")
+def fix_time_request():
+    t0= time.time()
+    yield
+    t1 = time.time()
+    logging.info("Test took %s seconds", t1 - t0)
